3c0b99e08848c98dc07a4505272d9d10e2693feb
Neil Larkin <neillarkin@gmail.com>
2019-05-12 14:16:23 +0000

    Setup authentication with API. Successfully retrieves artist info

--- js/main.js
+++ js/main.js
@@ -1,44 +1,43 @@
 function artistInformationHTML(artistData) {
-    // console.log(artistData.members[0]);
-    // console.log(Object.keys(artistData));
-    
-    Object.keys(artistData).forEach(function(key) {
-        console.log(artistData[key])
 
-    });
-    
+    artistData = artistData.results[0];
+    // console.log(artistData.id);
+    // console.log(artistData[0]);
+    // Object.keys(artistData).forEach(function(key) {
+    //         console.log(artistData.releases_url);
+    //          console.log(Object.keys(artistData));
+    // });
+
     return `
-        <h3>${artistData.name}
-            <span class="small-name">
-                (<a href="${artistData.uri}" target="_blank">${artistData.name}</a>)
-            </span>
+      <h3>${artistData.id}
+
         </h3>
-        <div class="dc-content">
-            <div class="dc-avatar">
-                <a href="${artistData.url}" target="_blank">
-                    <img src="${artistData.uri}" width="80" height="80" />
-                </a>
-            </div>
-             <p>Members: ${artistData.members['name']} <br> <a href="${artistData.releases_url}">Releases</a></p>
-        </div>`;
+      <h3>${artistData.title}
+        </h3>
+      
+        
+        
+        `;
 }
 
 // 11770
+// https://api.discogs.com/database/search?type=artist&q=${artist}&token=nBvZlBkjrlXGhxDUpVYiOKeRNHUdsBYffuasXHox
 
 function fetchDiscogsData(event) {
-     var artist = "11770";
-    // var artist = $("#dc-artistname").val();
+    //  var artist = "11770";
+    var artist = $("#dc-artistname").val();
     if (!artist) {
         $("#dc-artist-data").html(`<h3>Enter an Artist</h3>`);
         return;
     }
-
+    
+    // curl "https://api.discogs.com/database/search?q=Nirvana&token=nBvZlBkjrlXGhxDUpVYiOKeRNHUdsBYffuasXHox"
     $("#dc-artist-data").html(
         `<div id="loader">
             <img src="imgs/loader.gif" alt="loading..." />
         </div>`);
     $.when(
-        $.getJSON(`https://api.discogs.com/artists/${artist}`)
+        $.getJSON(`https://api.discogs.com/database/search?type=artist&q=${artist}&token=nBvZlBkjrlXGhxDUpVYiOKeRNHUdsBYffuasXHox`)
     ).then(
         function(response) {
             var artistData = response;
