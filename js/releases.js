2ad46940276da29e288ebbe7d6ade6beea7bbeaa
Neil Larkin <neillarkin@gmail.com>
2019-05-26 11:32:26 +0000

    Removed table cleaning functions and instead cleaned Json objects column & cell data

--- js/releases.js
+++ js/releases.js
@@ -18,21 +18,13 @@ function getURL() {
 function getTableHeaders(obj) {
     var tableHeaders = [];
 
+    // console.log(obj);
+
     Object.keys(obj).forEach(function(key) {
+
         tableHeaders.push(`<th>${key}</th>`);
     });
 
-
-    // if (tableHeaders[0] === "<th>stats</th>") {
-    //     tableHeaders.unshift("<th>Status</th>");
-    // }
-    // if (tableHeaders[3] === "<th>title</th>") {
-    //     tableHeaders.splice(3, 0, "<th>Format</th>");
-    //     console.log(tableHeaders);
-    // }
-
-
-    // let myHeader = tableHeaders.shift(tableHeaders.shift());
     return `<thead><tr class="text-uppercase">${tableHeaders}</tr></thead>`;
 
 }
@@ -51,9 +43,19 @@ function generatePaginationButtons(next, prev) {
     }
 }
 
+function Record(artist, title, year, format, label) {
+    this.artist = artist;
+    this.title = title;
+    this.year = year;
+    this.format = format;
+    this.label = label;
+}
+
+
 function writeToDocument(url) {
 
     var tableRows = [];
+    var cleanData = [{}];
     var el = document.getElementById("data");
     $("#data").html(`<div id="loader"><img src="imgs/loader.gif" alt="loading..." /></div>`);
 
@@ -66,95 +68,52 @@ function writeToDocument(url) {
 
         data = data.releases;
 
-        data.forEach(function(item) {
+        data.forEach(function(record) {
+            var rec = new Record(record.artist, record.title, record.year, record.format, record.label);
+              // console.log(Object.keys(rec));
+            // if (!(rec.key)) {
+            //     // console.log("No Info!");
+            //     rec.key = "No Info!";
+            // };
 
-            // delete item['status', 'stats', 'thumb', 'main_release', 'role', 'resource_url', 'type', 'id'];
 
-            delete item.status;
-            delete item.stats;
-            delete item.thumb;
-            delete item.main_release;
-            delete item.role;
-            delete item.resource_url;
-            delete item.type;
-            delete item.id;
-
-            if (item.format === undefined) {
-                item.format = "No format";
+            if (rec.format === undefined) {
+                rec.format = "No format";
             }
-            if (item.label === undefined) {
-                item.label = "No label";
+            if (rec.label === undefined) {
+                rec.label = "No label";
+            }
+            if (rec.year === undefined) {
+                rec.year = "No year";
             }
 
-            console.log(item);
+            cleanData.push(rec);
 
-            // console.log(item['stats']);
+        });
+
+
+        cleanData.forEach(function(Record) {
             var dataRow = [];
-            Object.keys(item).forEach(function(key) {
-
-                // if (item.format === undefined) {
-                //     return "#";
-                // }
-                // if (item.label === undefined) {
-                //     return "#";
-                // }
-
-                // if (item.status === undefined) {
-                //     return "accepted";
-                // }
 
 
-                var rowData = item[key].toString();
+            Object.keys(Record).forEach(function(key) {
+
+                var rowData = Record[key].toString();
+                // console.log(rowData);
+
                 var truncatedData = rowData.substring(0, 40);
 
-                dataRow.push(`<td>${truncatedData}</td>`);
+                dataRow.push(`<td>${truncatedData}</td>&nbsp`);
             });
-            // var myRow = dataRow.shift(dataRow.shift());
 
             tableRows.push(`<tr>${dataRow}</tr>`);
 
         });
-        var tableHeaders = getTableHeaders(data[0]);
+        var tableHeaders = getTableHeaders(cleanData[1]);
 
         el.innerHTML = `<table class="table-light table-striped" id="table-releases">${tableHeaders}${tableRows}</table>${pagination}`.replace(/,/g, "");;
-        // removeColumns();
-        // cleanColumns();
-
     });
 
 
 }
 
-// 0 1 2 6 8 10
-
-
-function cleanColumns() {
-
-    $.moveColumn = function(table, from, to) {
-        var rows = $('tr', table);
-        var cols;
-        rows.each(function() {
-            cols = $(this).children('th, td');
-            cols.eq(from).detach().insertBefore(cols.eq(to));
-        });
-    }
-
-    var tbl = $('table');
-    $.moveColumn(tbl, 9, 0);
-    $.moveColumn(tbl, 5, 1);
-    $.moveColumn(tbl, 8, 2);
-    $.moveColumn(tbl, 6, 3);
-    $.moveColumn(tbl, 7, 4);
-
-    // tbl = $('table tr');
-    var tbl2 = $('table tr');
-
-    tbl2.find('td:eq(11),th:eq(11)').remove();
-    tbl2.find('td:eq(10),th:eq(10)').remove();
-    tbl2.find('td:eq(9),th:eq(9)').remove();
-    tbl2.find('td:eq(8),th:eq(8)').remove();
-    tbl2.find('td:eq(7),th:eq(7)').remove();
-    tbl2.find('td:eq(6),th:eq(6)').remove();
-    tbl2.find('td:eq(5),th:eq(5)').remove();
-
-}
